872857204bb120a1954b0d425516b22a96c92495
maivananh2k02 <maihuyen2k02@gmail.com>
2021-06-09 22:48:54 +0700

    demo module 2

--- index.php
+++ index.php
@@ -2,21 +2,24 @@
 function sumItemArray($arr)
 {
     $sum = 0;
+    $count=0;
     for ($i = 0; $i < count($arr); $i++) {
         $sum += $arr[$i][$i];
+
     }
-    for ($j=count($arr)-1;$j>=0;$j--){
-        $sum+=$arr[$j][$j];
+    for ($j = count($arr) - 1; $j >= 0; $j--) {
+        $sum += $arr[$j][$count];
+        $count++;
     }
     return $sum;
 }
 
 $arr1 = [
     [1, 2, 3, 4, 5],
-    [1, 2, 3, 4, 5],
-    [1, 2, 3, 4, 5],
-    [1, 2, 3, 4, 5],
-    [1, 2, 3, 4, 5]
+    [1, 55, 3, 46, 5],
+    [1, 2, 100, 4, 5],
+    [1, 32, 3, 4, 5],
+    [1, 2, 3, 4, 10]
 ];
 echo sumItemArray($arr1);
 ?>
